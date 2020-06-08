5890909e9931bbcb70e5606386cafa1f95934b6d
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-06-08 15:41:14 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: [],
+    newToy: {
+      name: '',
+      image: ''
+    }
+  }
+
+
+  componentDidMount(){
+     fetch('http://localhost:3000/toys')
+     .then(res => res.json())
+     .then(toys => this.setState({ toys }))
   }
 
   handleClick = () => {
@@ -21,20 +33,81 @@ class App extends React.Component{
     })
   }
 
+  addLike = (id) => {
+   const likeToy = this.state.toys.find(toy => toy.id === id)
+   const toyCopy = this.state.toys.slice()
+   const index = toyCopy.indexOf(likeToy)
+   toyCopy[index].likes += 1
+   this.setState({ toys: toyCopy })
+
+   fetch('http://localhost:3000/toys' + '/' + id,{
+     method: 'PATCH', 
+     headers: {
+       'Content-Type': 'application/json',
+       Accept: 'application/json'
+     }, body: JSON.stringify({ 
+       likes: likeToy.likes
+     })
+   } )
+  }
+
+  deleteToy = (id) => {
+    this.setState({ toys: this.state.toys.filter(toy => toy.id !== id)})
+
+    fetch('http://localhost:3000/toys' + '/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+  }
+
+  handleOnchange = (event) => {
+    this.setState({ newToy: {...this.state.newToy, [event.target.name]: event.target.value } 
+   })
+  }
+
+  submitNewToy = (e) => {
+      e.persist()
+      e.preventDefault()
+      fetch('http://localhost:3000/toys', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }, body: JSON.stringify({
+          name: this.state.newToy.name,
+          image: this.state.newToy.image,
+          likes: 0
+        })
+      }).then(res => res.json())
+      .then(newToy => this.setState({
+        toys: [...this.state.toys, newToy]
+      }))
+      this.setState({
+        newToy: {
+          name: '',
+          image: ''
+        }
+      })
+  }
+
   render(){
+    console.log(this.state.newToy)
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm submitNewToy={this.submitNewToy}  name={this.state.newToy.name} image={this.state.newToy.image} handleOnchange={this.handleOnchange} />
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} addlike={this.addLike} deleteToy={this.deleteToy} />
       </>
     );
   }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  state = {
+    liked: false 
+  }
+
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.name}</h2>
+        <img src={this.props.image} alt={this.props.name} className="toy-avatar" />
+        <p>{this.props.likes} Likes </p>
+        <button onClick={() => this.props.addlike(this.props.id)} className="like-btn">Like {'<3'}</button>
+        <button onClick={()=> this.props.deleteToy(this.props.id)} className="del-btn">Donate to GoodWill</button>
       </div>
     );
   }
