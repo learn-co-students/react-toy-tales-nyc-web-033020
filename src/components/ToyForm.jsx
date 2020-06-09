79861c33c18e9c0f4504b232c2daa0de8817c9c3
lightfire224 <elias.taveras@columbia.edu>
2020-06-09 11:11:32 -0400

    Done.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state={
+    name: '',
+    image: ''
+  }
+
+  handleName = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  handleImage = (event) => {
+    this.setState({image: event.target.value})
+  }
+
   handleSubmit = (event) => {    
     event.persist()
     event.preventDefault()
 
-    const form = event.target
     const options = {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }, body: JSON.stringify({
-        name: form.name.value,
-        image: form.image.value,
+        name: this.state.name,
+        image: this.state.image,
         likes: 0
       })
     }
@@ -30,9 +42,9 @@ class ToyForm extends Component {
       <div className="container">
         <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" onChange={this.handleName} value={this.state.name} name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" onChange={this.handleImage}name="image" value={this.state.image}laceholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
