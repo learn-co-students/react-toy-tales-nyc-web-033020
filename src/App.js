d5e4e71a50230b6b813074aa7330640138399291
Garen Sahagian <g.s.sahagian@gmail.com>
2020-06-10 13:55:05 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,89 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
+// import data from './data'
 
 
 class App extends React.Component{
 
   state = {
+    toys: [],
     display: false
+  }
+
+  componentDidMount(){
+    this.fetchToys()
+  }
+  
+  fetchToys = () =>{
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(dbToys => this.renderToys(dbToys))
+  }
+
+  renderToys = (toysArray) =>{
+    this.setState({
+      toys:toysArray
+    })
+  }
+
+  addToy = (toy) =>{
+    this.setState(prevState =>({
+      toys: [...prevState.toys, toy]
+    })
+    )
+  }
+
+  likeToy = (toyId, newlikes) => {
+    // this.setState(prevState => {
+    //   const updatedToy = prevState.toys.find(toy => toy.id === toyId)
+    //   const updatedToyIndex = prevState.toys.findByIndex(toy => toy.id === toyId)
+    //   return {
+    //     ...prevState, toys:{
+    //       ...prevState.toys.filter(toy=> toy.id !== toyId), updatedToy:{
+    //         likes: newlikes
+    //       }
+    //     }
+    //   }
+    // }
+    // )
+
+    fetch(`http://localhost:3000/toys/${toyId}`,{
+      method: "PATCH",
+      headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: newlikes
+      })
+    })
+    .then(resp => resp.json())
+    .then(newToy => {
+      const oldToy = this.state.toys.find(toy=> toy.id === toyId)
+      const oldToyIndex = this.state.toys.indexOf(oldToy)
+      let updatedToys = [...this.state.toys]
+      updatedToys[oldToyIndex] = newToy
+      this.setState({
+        toys: updatedToys
+      })
+    })
+  }
+
+  deleteToy = (toyId) =>{
+    fetch(`http://localhost:3000/toys/${toyId}`,{
+      method: "DELETE",
+      headers:{ "Content-Type":"application/json",
+                Accept: "application/json"
+              }
+    })
+    .then(resp=> resp.json())
+    .then(toy=>{
+      this.setState(prevState=>({
+        toys: prevState.toys.filter(toy => toy.id !== toyId)
+      }))
+    })
+
   }
 
   handleClick = () => {
@@ -27,14 +103,14 @@ class App extends React.Component{
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm toys={this.state.toys} renderToys={this.renderToys} addToy={this.addToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} fetchToys={this.fetchToys} likeToy={this.likeToy} deleteToy={this.deleteToy}/>
       </>
     );
   }
