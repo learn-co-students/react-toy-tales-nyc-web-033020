4583aab6e353104d3e2f6ae4193b780ad9f404d5
github username <github email address>
2020-06-09 21:26:52 -0400

    Done.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target 
+    const newToy = {
+      name: form.name.value,
+      image: form.image.value,
+      likes: 0
+    }
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(res => res.json() )
+    .then( () => this.props.update() )
+    
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
           <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
